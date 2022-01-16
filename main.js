2b7ea03469002e151ec6295c96f38021feaa9c85
RanuRanjan <ranuranjan25@gmail.com>
2022-01-16 17:39:02 +0530

    local storage methods

---
+++ main.js
@@ -0,0 +1,35 @@
+
+// experiment:
+
+// 1:
+// localStorage.setItem("name","ranu")
+
+
+// var x = localStorage.getItem("name")
+// console.log(x);
+// var body=document.querySelector('body')
+// body.append(x)
+
+// var btn=document.querySelector("button")
+
+// function main(){
+//     return localStorage.clear()
+// }
+// // main()
+
+
+
+// 2:
+
+// arr
+var username =["ranu","ravi","yash","push","raushan"]
+// convert
+localStorage.setItem("username",username)
+
+
+
+// obj
+var obj={"name":"ranu","age":21}
+
+// convert
+localStorage.setItem("user",obj)
